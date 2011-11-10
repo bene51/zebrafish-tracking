effb1f0327efab3b7ee857b69cef7d24e4b6df98
Benjamin Schmid <bene.schmid@gmail.com>
2011-11-10 11:10:50 +0100

    Two plugins for sharing an open image over the network.

---
+++ huisken/network/ImageProvider.java
@@ -0,0 +1,62 @@
+package huisken.network;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ImageProvider implements PlugInFilter {
+
+	protected ImagePlus image;
+
+	public void run(int port) throws Exception {
+		ServerSocket serverSocket = new ServerSocket(port);
+		Socket clientSocket = serverSocket.accept();
+		ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		String inputLine;
+
+		while ((inputLine = in.readLine()) != null) {
+			if(inputLine.equals("close")) {
+				break;
+			}
+			if(inputLine.equals("getImage")) {
+				out.writeObject(getImage());
+			}
+		}
+		clientSocket.close();
+	}
+
+	public synchronized void setImage(ImagePlus image) {
+		this.image = image;
+	}
+
+	public synchronized ImagePlus getImage() {
+		return image;
+	}
+
+	@Override
+	public void run(ImageProcessor arg0) {
+		double n = IJ.getNumber("Port", 4444);
+		if(n == IJ.CANCELED)
+			return;
+		try {
+			run((int)n);
+		} catch(Exception e) {
+			IJ.error(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public int setup(String arg, ImagePlus imp) {
+		this.image = imp;
+		return DOES_ALL;
+	}
+}
---
+++ huisken/network/ImageReceiver.java
@@ -0,0 +1,58 @@
+package huisken.network;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.GenericDialog;
+import ij.plugin.PlugIn;
+
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ImageReceiver implements PlugIn {
+
+	private Socket socket;
+	private PrintWriter out;
+	private ObjectInputStream in;
+
+	@Override
+	public void run(String args) {
+		GenericDialog gd = new GenericDialog("ImageReceiver");
+		gd.addStringField("Host", "localhost");
+		gd.addNumericField("Port", 4444, 0);
+		gd.showDialog();
+		if(gd.wasCanceled())
+			return;
+		String host = gd.getNextString();
+		int port = (int)gd.getNextNumber();
+		try {
+			start(host, port);
+			getImage().show();
+			stop();
+		} catch(Exception e) {
+			IJ.error(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	public void start(String host, int port) throws Exception {
+		if(socket != null)
+			throw new RuntimeException("Already running");
+		socket = new Socket(host, port);
+		out = new PrintWriter(socket.getOutputStream(), true);
+		in = new ObjectInputStream(socket.getInputStream());
+	}
+
+	public ImagePlus getImage() throws Exception {
+		out.println("getImage");
+		return (ImagePlus)in.readObject();
+	}
+
+	public void stop() throws Exception {
+		out.println("close");
+		socket.close();
+		socket = null;
+		in = null;
+		out = null;
+	}
+}
