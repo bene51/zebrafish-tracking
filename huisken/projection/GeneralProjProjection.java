49863152067c3aace43b7ef79b8f5963d9c980e9
Benjamin Schmid <bene.schmid@gmail.com>
2011-12-06 14:21:10 +0100

    Use the lhlabs map projections.

---
+++ huisken/projection/GeneralProjProjection.java
@@ -0,0 +1,284 @@
+package huisken.projection;
+
+import ij.process.FloatProcessor;
+import ij.process.ImageProcessor;
+
+import java.awt.geom.GeneralPath;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.vecmath.Point3f;
+
+import com.jhlabs.map.Ellipsoid;
+import com.jhlabs.map.proj.Projection;
+
+
+public class GeneralProjProjection {
+
+	private SphericalMaxProjection smp;
+	private int w;
+	private int h;
+
+	private int minx, miny, maxx, maxy;
+
+	private int[][] vIndices;
+	private float[][] vertexWeights;
+
+	private final Projection projection;
+
+	public GeneralProjProjection(Projection proj) {
+		this.projection = proj;
+	}
+
+	public static GeneralPath readDatFile(String file) throws IOException {
+		BufferedReader buf = new BufferedReader(new FileReader(file));
+		GeneralPath path = new GeneralPath();
+		String line;
+		boolean closed = true;
+		int lineno = 0;
+		while((line = buf.readLine()) != null) {
+			lineno++;
+			line = line.trim();
+			if(line.equals("")) continue;
+			if(line.startsWith(">")) {
+				closed = true;
+				continue;
+			}
+			String[] toks = line.split("\\s");
+			try {
+				float x = Float.parseFloat(toks[0]);
+				float y = Float.parseFloat(toks[1]);
+				if(closed)
+					path.moveTo(x, y);
+				else
+					path.lineTo(x, y);
+			} catch(Exception e) {
+				throw new RuntimeException("Error parsing line " + lineno + ": " + line, e);
+			}
+			closed = false;
+		}
+		buf.close();
+		return path;
+	}
+
+	public int getWidth() {
+		return w;
+	}
+
+	public int getHeight() {
+		return h;
+	}
+
+	public GeneralPath createLines() {
+		GeneralPath lines = new GeneralPath();
+		double minLon = projection.getMinLongitudeDegrees();
+		double maxLon = projection.getMaxLongitudeDegrees();
+		double minLat = projection.getMinLatitudeDegrees();
+		double maxLat = projection.getMaxLatitudeDegrees();
+		int nLos = 10;
+		int nLas = 100;
+		for(int lo = 0; lo <= nLos; lo++) {
+			boolean first = true;
+			for(int la = 0; la < nLas; la++) {
+				float x = (lo - nLos/2) * (360.0f / nLos);
+				float y = (la - nLas/2) * (180.0f / nLas);
+				if(x < minLon || x > maxLon || y < minLat || y > maxLat)
+					continue;
+				if(first)
+					lines.moveTo(x, y);
+				else
+					lines.lineTo(x, y);
+				first = false;
+			}
+		}
+
+		nLos = 100;
+		nLas = 10;
+		for(int la = 0; la <= nLas; la++) {
+			boolean first = true;
+			for(int lo = 0; lo < nLos; lo++) {
+				float x = (lo - nLos/2) * (360.0f / nLos);
+				float y = (la - nLas/2) * (180.0f / nLas);
+				if(x < minLon || x > maxLon || y < minLat || y > maxLat)
+					continue;
+				if(first)
+					lines.moveTo(x, y);
+				else
+					lines.lineTo(x, y);
+				first = false;
+			}
+		}
+		return lines;
+	}
+
+	public void prepareForProjection(SphericalMaxProjection smp) {
+		this.smp = smp;
+
+		float globeRadius = 300f;
+		projection.setEllipsoid(new Ellipsoid("", globeRadius, globeRadius, 0.0D, ""));
+		projection.initialize();
+
+		if(!projection.hasInverse())
+			throw new RuntimeException("Inverse mapping missing");
+
+		minx = Integer.MAX_VALUE;
+		maxx = -minx;
+		miny = minx;
+		maxy = -minx;
+
+		double dlon = projection.getMaxLongitude() - projection.getMinLongitude();
+		double dlat = projection.getMaxLatitude() - projection.getMinLatitude();
+
+		Point2D.Double tmpin = new Point2D.Double();
+		Point2D.Double tmpout = new Point2D.Double();
+		for(int lo = 0; lo <= 100; lo++) {
+			for(int la = 0; la <= 100; la++) {
+				tmpin.x = (lo - 50) * dlon / 100 - 0.001;
+				tmpin.y = (la - 50) * dlat / 100 - 0.001;
+				projection.transformRadians(tmpin, tmpout);
+				minx = Math.min((int)tmpout.x, minx);
+				miny = Math.min((int)tmpout.y, miny);
+				maxx = Math.max((int)tmpout.x, maxx);
+				maxy = Math.max((int)tmpout.y, maxy);
+			}
+		}
+		System.out.println(maxx + " " + maxy);
+		System.out.println(minx + " " + miny);
+
+		this.w = maxx - minx + 1;
+		this.h = maxy - miny + 1;
+		System.out.println(w);
+		System.out.println(h);
+
+		vIndices = new int[w * h][3];
+		vertexWeights = new float[w * h][3];
+		Point3f p = new Point3f();
+		Point3f[] vertices = smp.getSphere().getVertices();
+		int index = 0;
+		for(int y = maxy; y >= miny; y--) {
+			for(int x = minx; x <= maxx; x++) {
+				Point2D.Double in = new Point2D.Double(x, y);
+				Point2D.Double out = new Point2D.Double();
+				out = projection.inverseTransformRadians(in, out);
+
+				double lon = out.x;
+				double lat = out.y;
+
+				if(lat >= projection.getMaxLatitude() ||
+					lat <= projection.getMinLatitude() ||
+					lon >= projection.getMaxLongitude() ||
+					lon <= projection.getMinLongitude()) {
+					index++;
+					continue;
+				}
+
+				double sLon = (float)Math.sin(lon);
+				double cLon = (float)Math.cos(lon);
+				double sLat = (float)Math.sin(lat);
+				double cLat = (float)Math.cos(lat);
+
+				smp.getPoint(sLon, cLon, sLat, cLat, p);
+
+				smp.getThreeNearestVertexIndices(p, vIndices[index]);
+
+				// interpolate according to distance
+				float d0 = 1 / p.distance(vertices[vIndices[index][0]]);
+				float d1 = 1 / p.distance(vertices[vIndices[index][1]]);
+				float d2 = 1 / p.distance(vertices[vIndices[index][2]]);
+				float sum = d0 + d1 + d2;
+				vertexWeights[index][0] = d0 / sum;
+				vertexWeights[index][1] = d1 / sum;
+				vertexWeights[index][2] = d2 / sum;
+
+				index++;
+			}
+		}
+	}
+
+	public GeneralPath transform(GeneralPath in) {
+		GeneralPath out = new GeneralPath();
+		PathIterator it = in.getPathIterator(null);
+		float[] seg = new float[6];
+
+		Point2D.Double din = new Point2D.Double();
+		Point2D.Double dout = new Point2D.Double();
+
+		while(!it.isDone()) {
+			int l = it.currentSegment(seg);
+			din.x = seg[0];
+			din.y = seg[1];
+			// clip to min/max long/lat
+			if(din.x < projection.getMinLongitudeDegrees()) din.x = projection.getMinLongitudeDegrees();
+			if(din.y < projection.getMinLatitudeDegrees()) din.y = projection.getMinLatitudeDegrees();
+			if(din.x > projection.getMaxLongitudeDegrees()) din.x = projection.getMaxLongitudeDegrees();
+			if(din.y > projection.getMaxLatitudeDegrees()) din.y = projection.getMaxLatitudeDegrees();
+			projection.transform(din, dout);
+			float x = (float)dout.x - minx;
+			float y = maxy - (float)dout.y;
+
+			if(l == PathIterator.SEG_MOVETO)
+				out.moveTo(x, y);
+			else
+				out.lineTo(x, y);
+			it.next();
+		}
+		return out;
+	}
+
+	public static void savePath(GeneralPath path, String file, int w, int h, boolean fill) throws IOException {
+		PrintWriter out = new PrintWriter(new FileWriter(file));
+		out.println("%%!PS-Adobe-1.0");
+		out.println("%%%%BoundingBox: 0 0 " + w + " " + h);
+		out.println("%%%%EndComments");
+
+		// Draw background:
+		out.println("0 0 moveto");
+		out.println(w + " 0 lineto");
+		out.println(w + " " + h + " lineto");
+		out.println("0 " + h + " lineto");
+		out.println("closepath");
+		out.println("0 setcolor");
+		out.println("fill");
+
+		out.println("0.3 setlinewidth");
+
+		PathIterator it = path.getPathIterator(null);
+		float[] seg = new float[6];
+		while(!it.isDone()) {
+			int l = it.currentSegment(seg);
+			if(l == PathIterator.SEG_MOVETO)
+				out.println(seg[0] + " " + seg[1] + " moveto");
+			else
+				out.println(seg[0] + " " + seg[1] + " lineto");
+			it.next();
+		}
+		out.println("1 setcolor");
+		if(fill)
+			out.println("fill");
+		else
+			out.println("stroke");
+		out.close();
+	}
+
+	public ImageProcessor project() {
+		FloatProcessor ip = new FloatProcessor(w, h);
+		float[] maxima = smp.getMaxima();
+		for(int y = 0; y < h; y++) {
+			for(int x = 0; x < w; x++) {
+				int index = y * w + x;
+
+				float v0 = vertexWeights[index][0] * maxima[vIndices[index][0]];
+				float v1 = vertexWeights[index][1] * maxima[vIndices[index][1]];
+				float v2 = vertexWeights[index][2] * maxima[vIndices[index][2]];
+
+				ip.setf(x, y, v0 + v1 + v2);
+			}
+		}
+		return ip;
+	}
+}
--- huisken/projection/Map_Projection.java
+++ huisken/projection/Map_Projection.java
@@ -5,37 +5,53 @@ import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.PlugIn;
 
+import java.awt.geom.GeneralPath;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.jhlabs.map.proj.AugustProjection;
+import com.jhlabs.map.proj.BonneProjection;
+import com.jhlabs.map.proj.GallProjection;
+import com.jhlabs.map.proj.Kavraisky7Projection;
+import com.jhlabs.map.proj.MercatorProjection;
+import com.jhlabs.map.proj.OrthographicAzimuthalProjection;
+import com.jhlabs.map.proj.WinkelTripelProjection;
+
 
 public class Map_Projection implements PlugIn {
 
-	public static final int MERCATOR   = 0;
-	public static final int GALLPETER  = 1;
-	public static final int FULLER     = 2;
-	public static final int POLAR      = 3;
-	public static final int KAVRAYSKIY = 4;
+	public static final int MERCATOR              = 0;
+	public static final int GALLPETER             = 1;
+	public static final int KAVRAYSKIY            = 2;
+	public static final int WINKEL_TRIPEL         = 3;
+	public static final int AUGUSTUS_EPICYCLOIDAL = 4;
+	public static final int BONNE                 = 5;
+	public static final int ORTHO_AZIMUTAL        = 6;
 
-	public static final String[] MAP_TYPES = new String[] {"Mercator", "Gall-Peter", "Fuller", "Polar transform", "Kavrayskiy"};
+	public static final String[] MAP_TYPES = new String[] {
+		"Mercator",
+		"Gall-Peter",
+		"Kavrayskiy",
+		"Winkel Tripel",
+		"Augustus Epicycloidal",
+		"Bonne",
+		"Orthogonal Azimutal"};
 
 	@Override
 	public void run(String arg) {
 		GenericDialogPlus gd = new GenericDialogPlus("Create 2D Maps");
-		gd.addDirectoryField("Data directory", "");
-		gd.addDirectoryField("Output directory", "");
-		gd.addChoice("Map type", MAP_TYPES, MAP_TYPES[0]);
-		gd.addNumericField("Target width", 700, 0);
+		gd.addDirectoryField("Data directory", "/Volumes/BENE/PostDoc/SphereProjection/registered");
+		gd.addDirectoryField("Output directory", "/Volumes/BENE/PostDoc/SphereProjection/registered");
+		gd.addChoice("Map type", MAP_TYPES, MAP_TYPES[3]);
 		gd.showDialog();
 		if(gd.wasCanceled())
 			return;
 		File datadir = new File(gd.getNextString());
 		File outputdir = new File(gd.getNextString());
 		int mapType = gd.getNextChoiceIndex();
-		int width = (int)gd.getNextNumber();
 
 		if(!datadir.isDirectory()) {
 			IJ.error(datadir + " is not a directory");
@@ -57,30 +73,42 @@ public class Map_Projection implements PlugIn {
 			return;
 		}
 		try {
-			createProjections(objfile.getAbsolutePath(), datadir.getAbsolutePath(), mapType, width, outputdir.getAbsolutePath());
+			createProjections(objfile.getAbsolutePath(), datadir.getAbsolutePath(), mapType, outputdir.getAbsolutePath());
 		} catch(Exception e) {
 			IJ.error(e.getMessage());
 			e.printStackTrace();
 		}
 	}
 
-	public void createProjections(String objfile, String datadir, int mapType, int width, String outputdir) throws IOException {
+	public void createProjections(String objfile, String datadir, int mapType, String outputdir) throws IOException {
 		SphericalMaxProjection smp = new SphericalMaxProjection(objfile);
-		createProjections(smp, datadir, mapType, width, outputdir);
+		createProjections(smp, datadir, mapType, outputdir);
 	}
 
-	public void createProjections(SphericalMaxProjection smp, String datadir, int mapType, int width, String outputdir) throws IOException {
-		MapProjection proj = null;
+	public void createProjections(SphericalMaxProjection smp, String datadir, int mapType, String outputdir) throws IOException {
+		GeneralProjProjection proj = null;
 		switch(mapType) {
-			case MERCATOR:   proj = new MercatorProjection();   break;
-			case GALLPETER:  proj = new GallPetersProjection(); break;
-			case FULLER:     proj = new FullerProjection();     break;
-			case POLAR:      proj = new PolarTransform();       break;
-			case KAVRAYSKIY: proj = new KavrayskiyProjection(); break;
+			case MERCATOR:              proj = new GeneralProjProjection(new MercatorProjection());   break;
+			case GALLPETER:             proj = new GeneralProjProjection(new GallProjection()); break;
+			case KAVRAYSKIY:            proj = new GeneralProjProjection(new Kavraisky7Projection()); break;
+			case WINKEL_TRIPEL:         proj = new GeneralProjProjection(new WinkelTripelProjection()); break;
+			case AUGUSTUS_EPICYCLOIDAL: proj = new GeneralProjProjection(new AugustProjection()); break;
+			case BONNE:                 proj = new GeneralProjProjection(new BonneProjection()); break;
+			case ORTHO_AZIMUTAL:        proj = new GeneralProjProjection(new OrthographicAzimuthalProjection()); break;
 			default: throw new IllegalArgumentException("Unsupported map type: " + mapType);
 		}
-		proj.prepareForProjection(smp, width);
+		proj.prepareForProjection(smp);
 
+		// create a postscript file with the coastline
+		GeneralPath coast = GeneralProjProjection.readDatFile("/Users/bschmid/PostDoc/paper/SphereProj/figure2/coast.dat");
+		coast = proj.transform(coast);
+		GeneralProjProjection.savePath(coast, outputdir + File.separator + "coastline.ps", proj.getWidth(), proj.getHeight(), true);
+
+		// create a postscript file with the lines
+		GeneralPath lines = proj.createLines();
+		lines = proj.transform(lines);
+		GeneralProjProjection.savePath(lines, outputdir + File.separator + "lines.ps", proj.getWidth(), proj.getHeight(), false);
+		
 		// collect files
 		List<String> tmp = new ArrayList<String>();
 		tmp.addAll(Arrays.asList(new File(datadir).list()));
@@ -97,7 +125,7 @@ public class Map_Projection implements PlugIn {
 			smp.loadMaxima(file);
 			String filename = new File(file).getName();
 			filename = filename.substring(0, filename.length() - 9) + ".tif";
-			IJ.save(new ImagePlus("Mercator", proj.project()), outputdir + File.separator + filename);
+			IJ.save(new ImagePlus("", proj.project()), outputdir + File.separator + filename);
 		}
 	}
 }
