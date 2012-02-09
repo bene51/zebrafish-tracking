74cff7dee7e51f42e5d17edc26529756575be508
Benjamin Schmid <bschmid@mpi-cbg.de>
2012-02-09 17:16:33 +0100

    New script for fitting the sphere, tailored to SPIM2.

---
+++ huisken/projection/FitSphereToTransmission.js
@@ -0,0 +1,78 @@
+importClass(Packages.javax.vecmath.Point3f);
+importClass(Packages.java.util.Properties);
+importClass(Packages.fiji.util.gui.GenericDialogPlus);
+importPackage(java.io);
+importPackage(java.net);
+
+gd = new GenericDialogPlus("");
+gd.addDirectoryField("Directory", "D:/SPIMdata/");
+gd.addNumericField("z spacing", 0, 5);
+gd.showDialog();
+
+
+folder = new java.lang.String(gd.getNextString()).replaceAll("\\\\", "/") + '/';
+System.out.println(folder);
+pw = 0.65;
+pd = gd.getNextNumber();
+
+properties = new Properties();
+properties.loadFromXML(new FileInputStream(folder + 'camera.xml'));
+w = Integer.parseInt(properties.getProperty("AOIWidth"));
+h = Integer.parseInt(properties.getProperty("AOIHeight"));
+System.out.println(w + "x" + h);
+/*
+s = new Socket("10.1.199.6", 1235);
+new PrintStream(s.getOutputStream()).println("z spacing");
+l = new BufferedReader(new InputStreamReader(s.getInputStream())).readLine();
+s.close();
+
+pw = 0.65;
+pd = Double.parseDouble(l);
+IJ.log("pd = " + pd);
+*/
+
+command = 
+	'run("Raw...", "open=' + folder + 'data/0000.dat image=[16-bit Unsigned] width=' + w + ' height=' + h + ' offset=0 number=1 gap=0 little-endian open")' +
+	'setVoxelSize(' + pw + ', ' + pw + ', ' + pd + ', "um"); rename("original");' +
+	'run("Scale...", "x=0.25 y=0.25 z=1.0 interpolation=Bicubic average process create title=image1");' +
+	'selectWindow("original");' +
+	'close();' +
+	'run("Duplicate...", "title=image2 duplicate stack");' +
+	'run("Gaussian Blur...", "sigma=1 stack");' +
+	'imageCalculator("Difference stack", "image1","image2");' +
+	'selectWindow("image2");' +
+	'close();' +
+	'waitForUser("Select a region and press ok");';
+IJ.runMacro(command);
+
+
+imp = IJ.getImage();
+d = imp.getStackSize();
+fs = IJ.runPlugIn("huisken.projection.Fit_Sphere", "");
+System.out.println(fs);
+
+center = new Point3f();
+fs.getCenter(center);
+System.out.println("***" + center.x);
+radius = fs.getRadius();
+
+
+props = new Properties();
+props.setProperty("w", Integer.toString(w));
+props.setProperty("h", Integer.toString(h));
+props.setProperty("d", Integer.toString(d));
+props.setProperty("pw", Double.toString(pw));
+props.setProperty("ph", Double.toString(pw));
+props.setProperty("pd", Double.toString(pd));
+props.setProperty("centerx", Float.toString(center.x));
+props.setProperty("centery", Float.toString(center.y));
+props.setProperty("centerz", Float.toString(center.z));
+props.setProperty("radius", Double.toString(radius));
+
+props.storeToXML(new FileOutputStream(folder + "SMP.xml"), "SphericalMaximumProjection");
+
+otherfolder = new java.lang.String(folder).replace("D:/SPIMdata", "U:");
+file = new File(otherfolder);
+if(!file.exists())
+	file.mkdirs();
+props.storeToXML(new FileOutputStream(otherfolder + "SMP.xml"), "SphericalMaximumProjection");
