649bb166e27d667a5389ee3fdb227461049d7739
Bene <bene.schmid@gmail.com>
2012-02-23 10:31:57 +0100

    Let the sphere projection be only triggered by the camera.
    No longer from within LV, via ports.

--- huisken/projection/TwoCamera_MaxProjection.java
+++ huisken/projection/TwoCamera_MaxProjection.java
@@ -9,13 +9,9 @@ import ij.process.ShortProcessor;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
@@ -30,7 +26,7 @@ import neo.BaseCameraApplication;
 
 public class TwoCamera_MaxProjection implements PlugIn {
 
-	public static final int PORT = 1236;
+//	public static final int PORT = 1236;
 	private File outputdir;
 
 	@Override
@@ -55,7 +51,8 @@ public class TwoCamera_MaxProjection implements PlugIn {
 		GenericDialogPlus gd = new GenericDialogPlus("Spherical_Max_Projection");
 		String[] cChoice = new String[2];
 		cChoice[TwoCameraSphericalMaxProjection.CAMERA1] = "Camera 1";
-		cChoice[TwoCameraSphericalMaxProjection.CAMERA2] = "Camera 2";gd.addDirectoryField("Output directory", defaultdir.getAbsolutePath());
+		cChoice[TwoCameraSphericalMaxProjection.CAMERA2] = "Camera 2";
+		gd.addDirectoryField("Output directory", defaultdir.getAbsolutePath());
 		gd.addChoice("Camera", cChoice, cChoice[0]);
 		gd.showDialog();
 		if(gd.wasCanceled())
@@ -136,7 +133,7 @@ public class TwoCamera_MaxProjection implements PlugIn {
 
 		int timepointStart = 0;
 		int timepointInc   = 1;
-		int nTimepoints    = timepoints;
+		nTimepoints    = timepoints;
 
 		toProcess = new short[w * h];
 
@@ -155,36 +152,36 @@ public class TwoCamera_MaxProjection implements PlugIn {
 	private CameraApp cameraApp;
 
 
-	private int w, h, d;
+	private int w, h, d, nTimepoints;
 	private short[] toProcess = null;
-	private int currentTimepoint = 0;
 	private static final boolean SAVE_RAW = false;
 
-	public void takeStack() {
+	private void startAcq() {
 		exec.execute(new Runnable() {
 			@Override
 			public void run() {
 				int d2 = 2 * d;
 				AT at = cameraApp.getAT();
-				at.AT_SetInt("FrameCount", d2);
-				at.AT_Command("AcquisitionStart");
-				long start = System.currentTimeMillis();
-				File tpDir = null;
-				if(SAVE_RAW) {
-					tpDir = new File(outputdir, String.format("tp%04d", currentTimepoint));
-					tpDir.mkdir();
+				for(int t = 0; t < nTimepoints; t++) {
+					at.AT_SetInt("FrameCount", d2);
+					at.AT_Command("AcquisitionStart");
+					long start = System.currentTimeMillis();
+					File tpDir = null;
+					if(SAVE_RAW) {
+						tpDir = new File(outputdir, String.format("tp%04d", t));
+						tpDir.mkdir();
+					}
+					for(int f = 0; f < d2; f++) {
+						at.AT_NextFrame(toProcess);
+						// ShortProcessor ip = new ShortProcessor(w, h, toProcess, null);
+						mmsmp.process(toProcess);
+						if(SAVE_RAW)
+							IJ.save(new ImagePlus("", new ShortProcessor(w, h, toProcess, null)), new File(tpDir, String.format("%04d.tif", f)).getAbsolutePath());
+					}
+					at.AT_Command("AcquisitionStop");
+					long end = System.currentTimeMillis();
+					System.out.println("Needed " + (end - start) + "ms  " + 1000f * d2 / (end - start) + " fps");
 				}
-				for(int f = 0; f < d2; f++) {
-					at.AT_NextFrame(toProcess);
-					// ShortProcessor ip = new ShortProcessor(w, h, toProcess, null);
-					mmsmp.process(toProcess);
-					if(SAVE_RAW)
-						IJ.save(new ImagePlus("", new ShortProcessor(w, h, toProcess, null)), new File(tpDir, String.format("%04d.tif", f)).getAbsolutePath());
-				}
-				currentTimepoint++;
-				at.AT_Command("AcquisitionStop");
-				long end = System.currentTimeMillis();
-				System.out.println("Needed " + (end - start) + "ms  " + 1000f * d2 / (end - start) + " fps");
 			}
 		});
 	}
@@ -198,24 +195,26 @@ public class TwoCamera_MaxProjection implements PlugIn {
 
 		at.AT_CreateBuffers();
 
-		ServerSocket server = new ServerSocket(PORT);
+		startAcq();
 
-		Socket client = server.accept();
-		BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-		String line = null;
-		System.out.println("listening");
-		while((line = in.readLine()) != null) {
-			System.out.println("***" + line + "***");
-			if(line.equals("STACK")) {
-				takeStack();
-			} else if(line.equals("DONE")) {
-				break;
-			}
-		}
-		System.out.println("closing");
-		in.close();
-		client.close();
-		server.close();
+//		ServerSocket server = new ServerSocket(PORT);
+//
+//		Socket client = server.accept();
+//		BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//		String line = null;
+//		System.out.println("listening");
+//		while((line = in.readLine()) != null) {
+//			System.out.println("***" + line + "***");
+//			if(line.equals("STACK")) {
+//				takeStack();
+//			} else if(line.equals("DONE")) {
+//				break;
+//			}
+//		}
+//		System.out.println("closing");
+//		in.close();
+//		client.close();
+//		server.close();
 	}
 
 	public void done() {
