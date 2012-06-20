453c7019ca239ccaac0a93f95a2c91a279ae64d2
Bene <bene.schmid@gmail.com>
2012-06-20 09:49:58 +0200

    More restructuring

--- huisken/projection/ResampleSMP.java
+++ huisken/projection/ResampleSMP.java
@@ -22,8 +22,8 @@ public class ResampleSMP implements PlugIn {
 		if(gd.wasCanceled())
 			return;
 
-		String datadir = gd.getNextString();
-		String outputdir = new File(datadir, "resampled").getAbsolutePath();
+		File datadir = new File(gd.getNextString());
+		File outputdir = new File(datadir, "resampled");
 		int f = (int)gd.getNextNumber();
 
 		try {
@@ -34,86 +34,106 @@ public class ResampleSMP implements PlugIn {
 		}
 	}
 
-	/*
-	 * Overall no of vertices = 12 * (Sum_{i=1}^{subd + 2}(i) - 3) + 12 - 30 * subd;
-	 *                        =
-	 */
-	public static void resample(String datadir, String outputdir, int f) throws IOException {
-		File dataf = new File(datadir);
-		String objfile = new File(dataf, "Sphere.obj").getAbsolutePath();
-		SphericalMaxProjection smp = new SphericalMaxProjection(objfile);
+	private File outputdir;
+	private File inputdir;
+	private File contribInDir, contribOutDir;
+	private SphericalMaxProjection smp;
+	private int factor;
+	private int subd;
+	IndexedTriangleMesh sphere;
+	int[] indices;
+
+	public void prepareResampling(File inputdir, File outputdir, int f) throws IOException {
+		this.inputdir = inputdir;
+		this.outputdir = outputdir;
+		this.factor = f;
+		this.smp = new SphericalMaxProjection(new File(inputdir, "Sphere.obj").getAbsolutePath());
 
 		int n = smp.getSphere().nVertices;
 		float radius = smp.getRadius();
 		Point3f center = smp.getCenter();
 
-		int subd = (int)((-20 + Math.sqrt(400 - 40 * (12 - n))) / 20 + 1);
-		subd /= f;
+		subd = (int)((-20 + Math.sqrt(400 - 40 * (12 - n))) / 20 + 1);
+		subd /= factor;
 
 		IJ.showStatus("Creating icosahedron");
 		Icosahedron icosa = new Icosahedron(radius);
 
 		IJ.showStatus("Creating buckyball");
-		IndexedTriangleMesh sphere = icosa.createBuckyball(radius, subd);
+		sphere = icosa.createBuckyball(radius, subd);
 		for(Point3f p : sphere.getVertices())
 			p.add(center);
 
 		IJ.showStatus("Creating indices");
-		int[] indices = new int[sphere.nVertices];
+		indices = new int[sphere.nVertices];
 		for(int i = 0; i < indices.length; i++)
 			indices[i] = smp.getNearestNeighbor(sphere.getVertices()[i]);
-		File outf = new File(outputdir);
-		if(outf.exists() && outf.list() != null) {
-			boolean cancelled = !IJ.showMessageWithCancel("Overwrite?", outputdir + " already exists. Overwrite?");
-			if(cancelled)
-				return;
-		}
-		outf.mkdir();
-		File contin = new File(dataf, "contributions");
-		File contout = new File(outf, "contributions");
-		contout.mkdir();
-		SphericalMaxProjection.saveSphere(sphere, new File(outf, "Sphere.obj").getAbsolutePath());
 
-		IJ.showStatus("Resampling timepoints");
-		File[] files = dataf.listFiles();
-		for(int i = 0; i < files.length; i++) {
-			File file = files[i];
-			String name = file.getName();
-			if(!name.endsWith(".vertices") || !name.startsWith("tp"))
-				continue;
-			File outfile = new File(outf, name);
-			if(outfile.exists())
-				continue;
+		if(!outputdir.exists())
+			outputdir.mkdir();
 
-			IJ.showProgress(i+1, files.length);
+		contribInDir = new File(inputdir, "contributions");
+		contribOutDir = new File(outputdir, "contributions");
+		contribOutDir.mkdir();
+		SphericalMaxProjection.saveSphere(sphere, new File(outputdir, "Sphere.obj").getAbsolutePath());
 
-			smp.loadMaxima(file.getAbsolutePath());
-			for(int j = 0; j < f; j++)
-				smp.smooth();
-
-			short[] maxima = new short[sphere.nVertices];
-			for(int j = 0; j < indices.length; j++)
-				maxima[j] = smp.getMaxima()[indices[j]];
-
-			SphericalMaxProjection.saveShortData(maxima, outfile.getAbsolutePath());
-
-			File contfile = new File(contin, name);
-			if(!contfile.exists())
-				continue;
-			int[] overlayold = SphericalMaxProjection.loadIntData(contfile.getAbsolutePath(), smp.getSphere().nVertices);
-			int[] overlaynew = new int[sphere.nVertices];
-			for(int j = 0; j < indices.length; j++)
-				overlaynew[j] = overlayold[indices[j]];
-			SphericalMaxProjection.saveIntData(overlaynew, new File(contout, name).getAbsolutePath());
-		}
-
-		File contfile = new File(dataf, "contributions.vertices");
+		// resample contributions if present
+		File contfile = new File(inputdir, "contributions.vertices");
 		if(!contfile.exists())
 			return;
 		int[] overlayold = SphericalMaxProjection.loadIntData(contfile.getAbsolutePath(), smp.getSphere().nVertices);
 		int[] overlaynew = new int[sphere.nVertices];
 		for(int j = 0; j < indices.length; j++)
 			overlaynew[j] = overlayold[indices[j]];
-		SphericalMaxProjection.saveIntData(overlaynew, new File(outf, "contributions.vertices").getAbsolutePath());
+		SphericalMaxProjection.saveIntData(overlaynew, new File(outputdir, "contributions.vertices").getAbsolutePath());
+	}
+
+	public void resampleTimepoint(int i) throws IOException {
+		String basename = String.format("tp%04d", i);
+		String name = basename + ".vertices";
+		File infile = new File(inputdir, name);
+		File outfile = new File(outputdir, name);
+		if(outfile.exists())
+			return;
+
+		smp.loadMaxima(infile.getAbsolutePath());
+		for(int j = 0; j < factor; j++)
+			smp.smooth();
+
+		short[] maxima = new short[sphere.nVertices];
+		for(int j = 0; j < indices.length; j++)
+			maxima[j] = smp.getMaxima()[indices[j]];
+
+		SphericalMaxProjection.saveShortData(maxima, outfile.getAbsolutePath());
+
+		File contfile = new File(contribInDir, name);
+		if(!contfile.exists())
+			return;
+		int[] overlayold = SphericalMaxProjection.loadIntData(contfile.getAbsolutePath(), smp.getSphere().nVertices);
+		int[] overlaynew = new int[sphere.nVertices];
+		for(int j = 0; j < indices.length; j++)
+			overlaynew[j] = overlayold[indices[j]];
+		SphericalMaxProjection.saveIntData(overlaynew, new File(contribOutDir, name).getAbsolutePath());
+	}
+
+	/*
+	 * Overall no of vertices = 12 * (Sum_{i=1}^{subd + 2}(i) - 3) + 12 - 30 * subd;
+	 *                        =
+	 */
+	public static void resample(File inputdir, File outputdir, int f) throws IOException {
+		ResampleSMP res = new ResampleSMP();
+		res.prepareResampling(inputdir, outputdir, f);
+
+		File[] files = inputdir.listFiles();
+		for(int i = 0; i < files.length; i++) {
+			File file = files[i];
+			String name = file.getName();
+			if(!name.endsWith(".vertices") || !name.startsWith("tp"))
+				continue;
+
+			int tp = Integer.parseInt(name.substring(2, 6));
+			res.resampleTimepoint(tp);
+			IJ.showProgress(i+1, files.length);
+		}
 	}
 }
